5c90acee4acde65602ba83155b6e8b431068e35b
simsonmoses <sims91502@gmail.com>
2023-08-29 16:08:50 +0530

    debug

--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -22,6 +22,31 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Sending message....");
         rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
+        rabbitTemplate.convertAndSend(DemoApplication.topicExchangeName,"foo.bar.baz","Hello from Rabbitmq!");
         receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
     }
 }
